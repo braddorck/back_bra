8de728583b3f4efaa44ad92d4c178c459745b49a
bradork <ecallallae@est.unap.edu.pe>
2024-07-01 18:46:51 -0400

    conect

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export const PORT = process.env.PORT || 3001
-export const MYSQLUSER  = process.env.MYSQLUSER || 'urvlyyxpvsanh5w6'
-export const MYSQLPASSWORD = process.env.MYSQLPASSWORD || 'QcN16dhJMuC0gF0JhMDF'
-export const MYSQLHOST = process.env.MYSQLHOST || 'b8zh0cpbkslszwu4tqc7-mysql.services.clever-cloud.com'
+export const MYSQLUSER  = process.env.MYSQLUSER || 'ubtokf5h2h2yoqhf'
+export const MYSQLPASSWORD = process.env.MYSQLPASSWORD || 'RYPgvGM2utqhqKmeIa2j'
+export const MYSQLHOST = process.env.MYSQLHOST || 'bktufztck4vwhff60pbb-mysql.services.clever-cloud.com'
 
-export const MYSQLDATABASE = process.env.MYSQLDATABASE || 'b8zh0cpbkslszwu4tqc7'
+export const MYSQLDATABASE = process.env.MYSQLDATABASE || 'bktufztck4vwhff60pbb'
 export const MYSQLPORT = process.env.MYSQLPORT || 3306
